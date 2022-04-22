502ffa0569d1d64fc56d0eca1b66f324aed71cc5
adubois31 <duboisaubry1110@gmail.com>
2022-04-22 11:24:22 +0200

    banc de registre implémenté mais non testé (c'est la merde)

--- microprocesseur/microprocesseur.sim/sim_1/behav/xsim/xsim.dir/Test_ALU_behav/webtalk/xsim_webtalk.tcl
+++ microprocesseur/microprocesseur.sim/sim_1/behav/xsim/xsim.dir/Test_ALU_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir /home/a_dubois/Bureau/4 I/Sys_info/microprocesseur/microprocesseur.sim/sim_1/behav/xsim/xsim.dir/Test_ALU_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Thu Apr 14 12:15:11 2022" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Fri Apr 22 11:12:43 2022" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2258646" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "LIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "bad28e37-53b7-41ed-9909-6b6937f1d1a1" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "85d1cba964a84c62bf386e640c18275c" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "40" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "43" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Ubuntu" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "Ubuntu 20.04.4 LTS" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-8700 CPU @ 3.20GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.02_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Memory -value "122608_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1046320628 -regid "" -xml /home/a_dubois/Bureau/4 I/Sys_info/microprocesseur/microprocesseur.sim/sim_1/behav/xsim/xsim.dir/Test_ALU_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/a_dubois/Bureau/4 I/Sys_info/microprocesseur/microprocesseur.sim/sim_1/behav/xsim/xsim.dir/Test_ALU_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/a_dubois/Bureau/4 I/Sys_info/microprocesseur/microprocesseur.sim/sim_1/behav/xsim/xsim.dir/Test_ALU_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_transmit -clientid 3626525984 -regid "" -xml /home/a_dubois/Bureau/4 I/Sys_info/microprocesseur/microprocesseur.sim/sim_1/behav/xsim/xsim.dir/Test_ALU_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/a_dubois/Bureau/4 I/Sys_info/microprocesseur/microprocesseur.sim/sim_1/behav/xsim/xsim.dir/Test_ALU_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/a_dubois/Bureau/4 I/Sys_info/microprocesseur/microprocesseur.sim/sim_1/behav/xsim/xsim.dir/Test_ALU_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
---
+++ microprocesseur/microprocesseur.srcs/sim_1/new/Test_Banc_Registres.vhd
@@ -0,0 +1,95 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 22.04.2022 10:25:17
+-- Design Name: 
+-- Module Name: Test_Banc_Registres - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Test_Banc_Registres is
+--  Port ( );
+end Test_Banc_Registres;
+
+architecture Behavioral of Test_Banc_Registres is
+
+COMPONENT Banc_Registres is
+    Port ( A : in STD_LOGIC_VECTOR (3 downto 0);
+           B : in STD_LOGIC_VECTOR (3 downto 0);
+           W : in STD_LOGIC_VECTOR (3 downto 0);
+           Write : in STD_LOGIC;
+           DATA : in STD_LOGIC_VECTOR (7 downto 0);
+           RST : in STD_LOGIC;
+           CLK : in STD_LOGIC;
+           QA : out STD_LOGIC_VECTOR (7 downto 0);
+           QB : out STD_LOGIC_VECTOR (7 downto 0));
+end COMPONENT;
+
+signal A_in : STD_LOGIC_VECTOR (3 downto 0);
+signal B_in : STD_LOGIC_VECTOR (3 downto 0);
+signal W_in : STD_LOGIC_VECTOR (3 downto 0);
+signal Write_in : STD_LOGIC;
+signal DATA_in : STD_LOGIC_VECTOR (7 downto 0);
+signal RST_in : STD_LOGIC;
+signal CLK_in : STD_LOGIC;
+signal QA_out : STD_LOGIC_VECTOR (7 downto 0);
+signal QB_out : STD_LOGIC_VECTOR (7 downto 0);
+
+constant CLK_period : time := 10ns;
+
+begin
+
+Label_uut: Banc_Registres port map (
+    A => A_in,
+    B => B_in,
+    W => W_in,
+    Write => Write_in,
+    DATA => DATA_in,
+    RST => RST_in,
+    CLK => CLK_in,
+    QA => QA_out,
+    QB => QB_out
+);
+
+
+CLK_process : process 
+    begin
+        CLK_in<= not (CLK_in);
+        wait for CLK_period/2;
+    end process;
+
+RST_in<='0','1' after 15 ns;
+Write_in <= '1' after 20 ns,'0' after 30 ns ;
+W_in <= X"3" after 20 ns, X"5" after 30 ns;
+DATA_IN <= X"09" after 19 ns, X"01" after 29 ns; 
+A_in <= X"3" after 22 ns, X"5" after 31ns;
+
+
+
+
+
+end Behavioral;
